499796f6e96de4b4f52fc9675026521e7aa5f06a
Kirk McCann <kjm206@pitt.edu>
2026-07-01 16:53:52 -0400

    Commit hello world for intro to git

---
+++ HelloWorld/HelloWorld.ino
@@ -0,0 +1,32 @@
+
+
+void setup() {
+  pinMode(A5, OUTPUT);
+  pinMode(A4, OUTPUT);
+  pinMode(A3, OUTPUT);
+  pinMode(9, INPUT);
+}
+
+
+void loop() {
+  if (digitalRead(9) == HIGH) {
+    digitalWrite(A5, HIGH);
+    delay(500);
+    digitalWrite(A5, LOW);
+    delay(500);
+
+    digitalWrite(A4, HIGH);
+    delay(500);
+    digitalWrite(A4, LOW);
+    delay(500);
+
+    digitalWrite(A3, HIGH);
+    delay(500);
+    digitalWrite(A3, LOW);
+    delay(500);
+  } else {
+    digitalWrite(A5, LOW);
+    digitalWrite(A4, LOW);
+    digitalWrite(A3, LOW);
+  }
+}
